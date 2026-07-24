589fddf7ea996b4b78aad314bd26e801df0a6822
Sandun Pushpika <sandun.pushpika123@gmail.com>
2026-07-25 02:27:44 +0530

    Added API KEY security

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, BackgroundTasks #type: ignore
+import os
+from fastapi import Depends, FastAPI, BackgroundTasks, HTTPException, Security, status #type: ignore
+from fastapi.security import APIKeyHeader #type: ignore
 
 from configs.database import SessionLocal
 from models.requests import RankRequest, ReviewRequest
@@ -10,8 +12,28 @@ from repositories.vehicle_embedding_repository import EmbeddingRepository
 
 app = FastAPI()
 
+API_KEY_NAME = "X-API-Key"
+API_KEY = os.getenv("API_KEY")
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
+
 embedding_service = VehicleEmbeddingService()
 
+def validate_api_key(api_key: str = Security(api_key_header)):
+    if not api_key:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="API Key missing",
+            headers={"WWW-Authenticate": API_KEY_NAME},
+        )
+
+    if api_key != API_KEY:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid API Key",
+            headers={"WWW-Authenticate": API_KEY_NAME},
+        )
+    
+    return api_key
 
 def get_manager():
     db = SessionLocal()
@@ -31,7 +53,8 @@ def health():
 @app.post("/api/review")
 def trigger_embedding_update(
     request: ReviewRequest,
-    background_tasks: BackgroundTasks
+    background_tasks: BackgroundTasks,
+    _: str = Depends(validate_api_key)
 ):
 
     manager = get_manager()
@@ -47,7 +70,7 @@ def trigger_embedding_update(
     }
 
 @app.post("/api/vehicles/rank")
-def rank_vehicles(request: RankRequest):
+def rank_vehicles(request: RankRequest, _: str = Depends(validate_api_key)):
 
     manager = get_manager()
 
